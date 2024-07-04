ae13a12a2da668a78437188c9a38fe82502cd03e
Raghunath Pradhan <raghunath.pradhan@cern.ch>
2024-07-04 19:03:27 +0200

    InJet BalanceFunction using CMS data

---
+++ quick_ccheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#root -l -b -q 'Tree_Analyzer.C("inputfile_PbPbMC_Jussi.txt", 0, "/eos/user/r/rpradhan/Research/ATUIC/Check_RootFile/Jussi/MC/rootfiles/quick_Check_Files", "PbPb", 1)'
+#root -l -b -q 'Tree_Analyzer.C("inputfile_PbPbMC_JUssiOne.txt", 0, "/eos/user/r/rpradhan/Research/ATUIC/Check_RootFile/Jussi/MC/rootfiles/quick_Check_Files", "PbPb", 1)'
+#root -l -b -q 'Tree_Analyzer.C("inputfile_PbPbMC_MAOD_Jussi.txt", 0, "/eos/user/r/rpradhan/Research/ATUIC/Check_RootFile/Jussi/MC/rootfiles/quick_Check_Files", "PbPb", 1)'
+#root -l -b -q 'Tree_Analyzer.C("inputfile_PbPbMC.txt", 0, "/eos/user/r/rpradhan/Research/ATUIC/Check_RootFile/Jussi/MC/rootfiles/quick_Check_Files", "PbPb", 1)'
+#root -l -b -q 'Tree_Analyzer.C("inputfile_PbPbMC_Vanderbilt.txt", 0, "/eos/user/r/rpradhan/Research/ATUIC/Check_RootFile/Jussi/MC/rootfiles/quick_Check_Files", "PbPb", 1)'
+
+#root -l -b -q 'Tree_Analyzer.C("inputfile_PbPbData.txt", 0, "/eos/user/r/rpradhan/Research/ATUIC/Check_RootFile/Jussi/Data/rootfiles/quick_Check_Files", "PbPb", 0)'
+#root -l -b -q 'Tree_Analyzer.C("inputfile_PbPb_MAODData.txt", 0, "/eos/user/r/rpradhan/Research/ATUIC/Check_RootFile/Jussi/Data/rootfiles/quick_Check_Files", "PbPb", 0)'
+
+root -l -b -q 'Tree_Analyzer.C("inputfile_ppMC_Jussi.txt", 0, "/eos/user/r/rpradhan/Research/AT_UIC_IITM/quick_Check_Files", "pp", 1)'
+#root -l -b -q 'Tree_Analyzer.C("inputfile_pprefData.txt", 0, "/eos/user/r/rpradhan/Research/AT_UIC_IITM/quick_Check_Files", "pp", 0)'
+#root -l -b -q 'Tree_Analyzer.C("inputfile_pprefData_Jussi.txt", 0, "/eos/user/r/rpradhan/Research/AT_UIC_IITM/quick_Check_Files", "pp", 0)'
+#root -l -b -q 'Tree_Analyzer.C("inputfile_pprefData.txt", 0, "/eos/user/r/rpradhan/Research/ATUIC/Check_RootFile/Jussi/Data/rootfiles/quick_Check_Files", "PbPb", 0)'
+
